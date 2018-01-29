92e2cd76fd6b779a4d8ecfa059b50a34c5053b9f
zhutianyu <415960202@qq.com>
2018-01-29 17:03:12 +0800

    add use without npm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Scrollbar from './components/main.js'
 import './styles/less/index.less'
 
+const install = function (Vue, opts = {}) {
+  if (install.installed) return
+  Vue.component('VmScrollbar', Scrollbar)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  Vue.component('VmScrollbar', Scrollbar)
+}
+
+Scrollbar.install = install
+
 export default Scrollbar
